e6f2898e0dda927c91b36d5603f78239d102de15
Behets W. Arthur <arthur.behets24@gmail.com>
2021-11-24 11:25:56 +0100

    following navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// TODO header appear when body translate-x
+// TODO header appear when body translate-y
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50){
+        document.getElementById('main').style.paddingTop ="20px";
+        document.getElementById('navbar').style.display ="none";
+        document.getElementById('navbar').style.position ="fixed";
+        setTimeout(function(){ 
+        document.getElementById('navbar').style.display =""; ; }, 3000);
+    }
+    else{
+        document.getElementById('main').style.paddingTop ="";
+        document.getElementById('navbar').style.display ="";
+        document.getElementById('navbar').style.position ="";
+    }
+}
